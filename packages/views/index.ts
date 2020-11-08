8ca8c7f0fe478f24ebd261b78dc35bb46f0bb5e9
fernandopadangg <michael.fernando@ottencoffee.co.id>
2020-11-09 01:06:38 +0700

    add new pages and views stock-outbound

--- packages/views/index.ts
+++ packages/views/index.ts
@@ -1,3 +1,4 @@
 export { CategoryView } from './src/category';
 export { ErrorView } from './src/error';
 export { HomeView } from './src/home';
+export { StockOutboundView } from './src/stock-outbound';
---
+++ packages/views/src/stock-outbound/container.tsx
@@ -0,0 +1,5 @@
+export default function Container() {
+  return(
+    <h2>views Stock Outbound</h2>
+  );
+}
