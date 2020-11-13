a6eb7a90849c1b6d4c2d4529a2f49c57673f156c
fernandopadangg <michael.fernando@ottencoffee.co.id>
2020-11-14 00:34:01 +0700

    delete unused code

--- packages/helper/index.ts
+++ packages/helper/index.ts
@@ -1,3 +1,2 @@
 export { default as ServiceSSR } from './src/service-ssr';
 export { default as Sanitize } from './src/sanitize';
-export { default as withAuthentication } from './src/authentication';
