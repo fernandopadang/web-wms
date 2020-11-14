c285b2101758e41767bf948b040cabff6e1a468e
fernandopadangg <michael.fernando@ottencoffee.co.id>
2020-11-14 12:59:24 +0700

    update url mainImage seo page

--- packages/views/src/seo-page/index.tsx
+++ packages/views/src/seo-page/index.tsx
@@ -12,7 +12,7 @@ const SeoPage = (props: {type?: string}) => {
   const landingDescription = "Hello! My name is Michael Fernando Padang. A career in Information Technology, specifically in the areas of application development and IT consulting that will optimally utilize strong planning skills, analytical skills as well as JavaScript, Typescript, PHP programming skills, SQL database skill, and Business Process Analysis in order to work individually or teamwork, leadership skills, time-management and communications skills.";
   let breadcrumbs;
   let mainImage;
-  mainImage = props.type === "LANDING" ? "https://i.ibb.co/vZF4pxH/circle-cropped.png" : "https://i.ibb.co/5rdvW9F/iconfinder-technologymachineelectronicdevice24-4026436-113326.png";
+  mainImage = props.type === "LANDING" ? "https://i.ibb.co/Jp8Cpj9/output-onlinepngtools.png" : "https://i.ibb.co/5rdvW9F/iconfinder-technologymachineelectronicdevice24-4026436-113326.png";
   description = props.type === "LANDING" ? landingDescription : description;
   breadcrumbs =  [
     {
@@ -42,7 +42,7 @@ const SeoPage = (props: {type?: string}) => {
       <BreadcrumbJsonLd itemListElements={breadcrumbs}/>
       <h1>{title}</h1>
       <span>{description}</span>
-      {props.type === "LANDING" && <img src="https://i.ibb.co/vZF4pxH/circle-cropped.png" alt="Michael Fernando Padang" width="20%" />}
+      {props.type === "LANDING" && <img src="https://i.ibb.co/Jp8Cpj9/output-onlinepngtools.png" alt="Michael Fernando Padang" width="20%" />}
     </div>
   );
 };
