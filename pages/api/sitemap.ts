aa11deaf3710b6fe3355cc64e1a672bcf4ede534
fernandopadangg <michael.fernando@ottencoffee.co.id>
2020-11-15 01:13:34 +0700

    add api generate sitemap and robots.txt

---
+++ pages/api/sitemap.ts
@@ -0,0 +1,43 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+export default async (req: any, res: any) => {
+  try {
+    const smStream = new SitemapStream({
+      hostname: `https://${req.headers.host}`,
+    });
+
+    // List of sitemap
+    const pages: any[] = [
+      {
+        slug: '/landing'
+      },
+      {
+        slug: '/login'
+      }
+    ];
+
+    // Create each URL row
+    pages.forEach(page => {
+      smStream.write({
+        url: page.slug,
+        changefreq: 'daily',
+        priority: 0.9
+      });
+    });
+
+    // End sitemap stream
+    smStream.end();
+
+    // XML sitemap string
+    const sitemapOutput = (await streamToPromise(smStream)).toString();
+
+    // Change headers
+    res.writeHead(200, {
+      'Content-Type': 'application/xml'
+    });
+
+    // Display output to user
+    res.end(sitemapOutput);
+  } catch(e: any) {
+    res.send(JSON.stringify(e));
+  }
+};
